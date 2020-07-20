74cccc7c8f86613b8667ea68eabf44c04ffc3b98
Brice Le Gallo <lebowvsky@gmail.com>
2020-07-20 10:21:17 +0200

    CapitalizeFirstLetter done

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -5,3 +5,12 @@ assert.strictEqual(capitalizeFirstLetters("javaScript is so good"), "Javascript 
 assert.strictEqual(capitalizeFirstLetters("z"), "Z");
 assert.strictEqual(capitalizeFirstLetters(""), "");
 
+const capitalizeFirstLetters = (input) => {
+  let arrTemp = input.split(" ");
+  arrTemp.forEach(word => {
+    word.length > 0
+    ? word[0].toUpperCase() + word.slice(1)
+    : '';
+  })
+  return arrTemp.join(" ");
+}
