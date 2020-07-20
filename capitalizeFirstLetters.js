85a182117c5ed02455b0d500ee4622cce7054081
Brice Le Gallo <lebowvsky@gmail.com>
2020-07-20 10:42:06 +0200

    function done

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,16 +1,20 @@
 const assert = require("assert");
 
+const capitalizeFirstLetters = (input) => {
+  let arrTemp = input.split(" ");
+  let arrSoluce =[];
+  if(arrTemp[0]) {
+    input.split(" ").forEach(word => {
+      arrSoluce.push(word[0].toUpperCase() + word.slice(1))
+    })
+  }
+  return arrSoluce.join(" ");
+}
+
+/* console.log(capitalizeFirstLetters("")) */
+
 assert.strictEqual(typeof capitalizeFirstLetters, "function");
-assert.strictEqual(capitalizeFirstLetters("javaScript is so good"), "Javascript Is So Good");
+assert.strictEqual(capitalizeFirstLetters("javaScript is so good"), "JavaScript Is So Good");
 assert.strictEqual(capitalizeFirstLetters("z"), "Z");
 assert.strictEqual(capitalizeFirstLetters(""), "");
 
-const capitalizeFirstLetters = (input) => {
-  let arrTemp = input.split(" ");
-  arrTemp.forEach(word => {
-    word.length > 0
-    ? word[0].toUpperCase() + word.slice(1)
-    : '';
-  })
-  return arrTemp.join(" ");
-}
